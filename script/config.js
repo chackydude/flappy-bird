a1bab893ced9728af1ba5d036ab4b3170e88435a
Daniil Trofimov <trofimovdanil946@gmail.com>
2021-02-06 18:22:08 +0300

    some fixes

--- script/config.js
+++ script/config.js
@@ -1,12 +1,12 @@
 export let config = {
     pipe: [], // position of the bird
-    gap: 100, // px, gap between pipeUp & pipeDown
+    gap: 200, // px, gap between pipeUp & pipeDown
 
     // bird starting coordinates
     xPos : 10,
     yPos : 150,
 
-    grav : 2.65, // OY coord. changing
+    grav : 2.5, // OY coord. changing
     score : 0,
     step : 2, // OX coord. changing
 
